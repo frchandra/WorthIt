00fea6f6bd8e0cddfb50b543bef96664e9869f99
frchandra <nismara.chandra@gmail.com>
2021-04-24 15:27:16 +0700

    mencoba mengaplikasikan CRUDE yey...

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorthIt.Models;
+
+namespace WorthIt.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option): base(option)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WorthIt.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WorthIt
 {
@@ -23,6 +25,9 @@ namespace WorthIt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                 Configuration.GetConnectionString("DefaultConnection")
+                 ));
             services.AddControllersWithViews();
         }
 
